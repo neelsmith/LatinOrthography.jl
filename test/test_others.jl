bf66c8e4421af7484800130f981377f25de8539a
Neel Smith <dnsmith.neel@gmail.com>
2024-02-04 04:10:11 -0500

    Update pkgs, test Latin24

--- test/test_others.jl
+++ test/test_others.jl
@@ -4,6 +4,14 @@
     @test typeof(latin) == Latin24
     @test supertype(Latin24) == LatinOrthographicSystem
 
+    ttypes = tokentypes(latin)
+    @test length(ttypes) == 3
 
-    
+    @test validcp("i", latin)
+    @test validcp("j", latin) == false
+    @test validcp("v", latin)
+    @test validcp("u", latin)
+
+    @test validstring("vocauit", latin)
+    @test validstring("Juno", latin) == false
 end
